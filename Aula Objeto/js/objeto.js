ef9b3c38d787d0960af53b7191fd1fbb5ddddf09
richardcamargo07 <richardcamargo1b@gmail.com>
2024-10-07 21:45:53 -0300

    Add files via upload

---
+++ Aula Objeto/js/objeto.js
@@ -0,0 +1,16 @@
+var objpf = {
+    nome: "Richard",
+    idade: 18,
+    Profissão: "Desempregado",
+    trabalhando: false,
+
+
+};
+
+console.log(objpf.nome);
+//Tipo de dado typeof
+console.log(typeof objpf.trabalhando);
+
+//alterar valor da variável
+objpf.idade = 19;
+console.log(objpf.idade);
